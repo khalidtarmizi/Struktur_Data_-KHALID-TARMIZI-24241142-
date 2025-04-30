d1a519650968f697c772bc5f610e079c0483b06a
khalid tarmizi-alprog-D <khalidtarmizi01@gmail.com>
2025-04-30 10:17:11 +0800

    Add files via upload

---
+++ tugas 2.py
@@ -0,0 +1,12 @@
+print ("KHALID TARMIZI 24241142")
+inputuser = int(input("masukkan bilangan antara 24 atau lebih dari 42"))
+
+#+++++24------------
+#memeriksa angka yang kurang dari 24
+iskurangdari = (inputuser <24)
+
+#memeriksa angka yang kurang dari 42
+islebihdari = (inputuser <42)
+
+final = iskurangdari or islebihdari
+print("angka yang anda masukan", final)
